ab8c41027b279f929a6db40d46ea797f9a5672c4
Leonid Nikolaev <nikonor@nikonor.ru>
2020-08-31 15:51:55 +0300

    [v] перевод на match

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,25 @@ fn main() {
 
 fn call(c: DoWhat){
     let cmd = c.cmd.unwrap();
-    if  cmd =="TRAIN" {
-        println!("\tИщем электрички из {} в {}", c.params[0].as_str(), c.params[1].as_str());
-        let r = match get_train(c.params[0].to_string(), c.params[1].to_string()) {
-            Ok(s) => s,
-            Err(e) => format!("Ошибка:{}", e.to_string())
-        };
-        println!("\t{}",r);
-    } else if cmd =="ALARM" {
-        println!("\tСтавим будильник на {}",c.params[0].as_str());
-        let r = match set_alarm(c.params[0].to_string()) {
-            Ok(s) => s,
-            Err(e) => format!("Ошибка:{}", e.to_string())
-        };
-        println!("\t{}",r);
-    } else {
-        println!("\n\nНе знаю что делать\n\n")
-    }
+
+    match cmd.as_str() {
+        "TRAIN" => {
+            println!("\tИщем электрички из {} в {}", c.params[0].as_str(), c.params[1].as_str());
+            let r = match get_train(c.params[0].to_string(), c.params[1].to_string()) {
+                Ok(s) => s,
+                Err(e) => format!("Ошибка:{}", e.to_string())
+            };
+            println!("\t{}",r);
+        },
+        "ALARM" => {
+            println!("\tСтавим будильник на {}",c.params[0].as_str());
+            let r = match set_alarm(c.params[0].to_string()) {
+                Ok(s) => s,
+                Err(e) => format!("Ошибка:{}", e.to_string())
+            };
+            println!("\t{}",r);
+        },
+        _ => println!("\n\nНе знаю что делать\n\n")
+    };
 
 }
