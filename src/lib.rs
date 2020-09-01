d5e223dbba462cef0a84a6342985d5ae07731da5
Leonid Nikolaev <nikonor@nikonor.ru>
2020-09-01 12:02:06 +0300

    [v] работает цикл

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,27 @@
 use std::fmt::Error;
 
-pub struct DoWhat{
+pub struct DoWhat {
     pub cmd: Option<String>,
-    pub params: Vec<String>
+    pub params: Vec<String>,
 }
 
-pub fn parse_cmd(args: Vec<String>) -> DoWhat{
-    let cmd = args[1].to_string();
+pub fn parse_cmd(args: Vec<String>) -> DoWhat {
+    let cmd = args[0].to_string();
 
-    DoWhat{cmd: Some(cmd.to_uppercase()),  params: args[2..].to_vec() }
+    let ret = DoWhat {
+        cmd: Some(cmd.to_uppercase()),
+        params: args[1..].to_vec(),
+    };
+
+    return ret;
 }
 
-pub fn get_train(from:String, to: String) -> Result<String, Error> {
+pub fn get_train(from: String, to: String) -> Result<String, Error> {
     println!("\t\tВызов get_train для {}-{}", from, to);
     Ok("Ok".to_string())
 }
 
-pub fn set_alarm(time:String) -> Result<String, Error> {
+pub fn set_alarm(time: String) -> Result<String, Error> {
     println!("\t\tВызов set_alarm для {}", time);
     Ok("Ok".to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,65 @@
-use std::env;
 use major::*;
+// use std::env;
+use std::io;
 
 fn main() {
     println!("Старт");
-    call(parse_cmd(env::args().collect()));
+
+    print!("Введите команду: ");
+    loop {
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+
+        // println!(
+        //     "command={}",
+        //     command.split_ascii_whitespace().enumerate().collect()
+        // );
+        if call(parse_cmd(
+            command
+                .split_ascii_whitespace()
+                .map(|s| s.to_string())
+                .collect(),
+        )) {
+            break;
+        }
+        // call(parse_cmd(env::args().collect()));
+    }
     println!("Финиш");
 }
 
-fn call(c: DoWhat){
+fn call(c: DoWhat) -> bool {
     let cmd = c.cmd.unwrap();
+    let mut ret = false;
 
     match cmd.as_str() {
         "TRAIN" => {
-            println!("\tИщем электрички из {} в {}", c.params[0].as_str(), c.params[1].as_str());
+            println!(
+                "\tИщем электрички из {} в {}",
+                c.params[0].as_str(),
+                c.params[1].as_str()
+            );
             let r = match get_train(c.params[0].to_string(), c.params[1].to_string()) {
                 Ok(s) => s,
-                Err(e) => format!("Ошибка:{}", e.to_string())
+                Err(e) => format!("Ошибка:{}", e.to_string()),
             };
-            println!("\t{}",r);
-        },
+            println!("\t{}", r);
+        }
         "ALARM" => {
-            println!("\tСтавим будильник на {}",c.params[0].as_str());
+            println!("\tСтавим будильник на {}", c.params[0].as_str());
             let r = match set_alarm(c.params[0].to_string()) {
                 Ok(s) => s,
-                Err(e) => format!("Ошибка:{}", e.to_string())
+                Err(e) => format!("Ошибка:{}", e.to_string()),
             };
-            println!("\t{}",r);
-        },
-        _ => println!("\n\nНе знаю что делать\n\n")
+            println!("\t{}", r);
+        }
+        "QUIT" => {
+            println!("просят выйти");
+            ret = true;
+        }
+        _ => println!("\n\nНе знаю что делать: {}\n\n", cmd.as_str()),
     };
 
+    ret
 }
